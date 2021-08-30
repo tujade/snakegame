2b9dec774680dd7261b8ca5a585750e8462ff3f1
tujade <jade.mundim@gmail.com>
2021-08-30 20:45:03 -0300

    Finalização do Jogo no JavaScript

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ snake[0] = {
 }
 let direction = "right";
 let food = {
-    x: Math.floor(Math.random() * 15 + 1) * box, 
-    y: Math.floor(Math.random() * 15 + 1) * box
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
 };
 
 // Background
@@ -21,7 +21,7 @@ function criarBG() {
 
 // Cobrinha
 function criarCobrinha() {
-  for(i=0; i < snake.length; i++) {
+  for (i = 0; i < snake.length; i++) {
     context.fillStyle = "black";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
@@ -38,20 +38,28 @@ document.addEventListener('keydown', update);
 
 
 // Função Movimento a partir do teclado
-function update (event) {
-  if(event.keyCode == 37 && direction != "right") direction = "left";
-  if(event.keyCode == 38 && direction != "down") direction = "up";
-  if(event.keyCode == 39 && direction != "left") direction = "right";
-  if(event.keyCode == 40 && direction != "up") direction = "down";
+function update(event) {
+  if (event.keyCode == 37 && direction != "right") direction = "left";
+  if (event.keyCode == 38 && direction != "down") direction = "up";
+  if (event.keyCode == 39 && direction != "left") direction = "right";
+  if (event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 // Iniciar Jogo 
 function iniciarJogo() {
   //Regra para que a cobrinha não suma da tela ao passar do limite
-   if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-   if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-   if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-   if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+  // Encerrar o jogo se o a cabeça se chocar com o corpo
+  for(i = 1; i < snake.length; i++) {
+      if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+        clearInterval(jogo);
+        alert('Você perdeu! :(')
+      }
+  }
 
   criarBG();
   criarCobrinha();
@@ -60,18 +68,18 @@ function iniciarJogo() {
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
-  if(direction == "right") snakeX += box;
-  if(direction == "left") snakeX -= box;
-  if(direction == "up") snakeY -= box;
-  if(direction == "down") snakeY += box;
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
 
   //Condição para a cobrinha crescer ou não
-  if(snakeX != food.x || snakeY != food.y) {
+  if (snakeX != food.x || snakeY != food.y) {
     snake.pop();
 
   } else {
-       food.x = Math.floor(Math.random() * 15 + 1) * box;
-       food.y = Math.floor(Math.random() * 15 + 1) * box;
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
   }
 
 
@@ -83,7 +91,4 @@ function iniciarJogo() {
   snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 100); 
-
-
-
+let jogo = setInterval(iniciarJogo, 100);
