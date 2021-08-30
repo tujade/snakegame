3287e56d5abd860db645878e68830b930e3686e1
tujade <jade.mundim@gmail.com>
2021-08-30 19:45:22 -0300

    Criação/definição dos movimentos

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variáveis
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
@@ -6,6 +7,7 @@ snake[0] = {
   x: 8 * box,
   y: 8 * box,
 }
+let direction = "right";
 
 // Background
 function criarBG() {
@@ -21,7 +23,30 @@ function criarCobrinha() {
   }
 }
 
-criarBG();
-criarCobrinha();
+// Iniciar Jogo 
+function iniciarJogo() {
+  criarBG();
+  criarCobrinha();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if(direction == "right") snakeX += box;
+  if(direction == "left") snakeX -= box;
+  if(direction == "up") snakeY -= box;
+  if(direction == "down") snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
 
 
