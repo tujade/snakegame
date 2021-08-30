cb340f615bedc66f28af7bad8009a61c30b56ecf
tujade <jade.mundim@gmail.com>
2021-08-30 20:51:14 -0300

    Finalização do Projeto e Estilização

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function iniciarJogo() {
   for(i = 1; i < snake.length; i++) {
       if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
         clearInterval(jogo);
-        alert('Você perdeu! :(')
+        alert('Você perdeu! :( Recarregue a página e continue jogando!')
       }
   }
 
