3490b13319f5e17ac3bb20b56cc13f0f260c612f
tujade <jade.mundim@gmail.com>
2021-08-30 19:34:20 -0300

    Criação da cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box,
+}
 
+// Background
 function criarBG() {
   context.fillStyle = 'lightgreen';
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+// Cobrinha
+function criarCobrinha() {
+  for(i=0; i < snake.length; i++) {
+    context.fillStyle = "black";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
 criarBG();
+criarCobrinha();
+
+
